7ec0350f003da6c8564a1f83473e020a0d1af5a9
devpark <devpark29@gmail.com>
2024-02-14 14:28:00 +0900

    before install lucide-react

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,16 @@
+// Installed with shadcn
+import { Medal } from "lucide-react";
+
+const MarketingPage = () => {
+  return (
+    <div className="flex items-center justify-center flex-col">
+      <div className="flex items-center justify-center flex-col">
+        <div>
+          <Medal />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MarketingPage;
