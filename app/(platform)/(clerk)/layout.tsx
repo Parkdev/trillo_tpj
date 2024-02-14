d9a3127af524d2d7e0b5893d1aae28dbff9a5680
devpark <devpark29@gmail.com>
2024-02-15 00:29:53 +0900

    test auth with clerk lib

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,7 @@
+const SignLayout = ({ children }: { children: React.Reactnode }) => {
+  return (
+    <div className="h-full flex items-center justify-center">{children}</div>
+  );
+};
+
+export default SignLayout;
---
+++ middleware.ts
@@ -0,0 +1,13 @@
+// 사용자 인증 및 관리를 위한 미들웨어 설정
+import { authMiddleware } from "@clerk/nextjs";
+
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
+export default authMiddleware({
+  publicRoutes: ["/"],
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
