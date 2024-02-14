ebe9eeac94a1de1b93de9b70216ae602a9077ddf
devpark <devpark29@gmail.com>
2024-02-14 22:18:55 +0900

    add metadata

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -39,8 +39,7 @@ const MarketingPage = () => {
       </div>
       <div
         className={cn(
-          "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
-          textFont.className
+          "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto"
         )}
       >
         Collaborate, manage projects, and reach new productivity peaks. From
