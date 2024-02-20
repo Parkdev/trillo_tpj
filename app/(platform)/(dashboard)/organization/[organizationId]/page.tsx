20f16af6db62f96294a6122646ad00c20c7c5be3
devpark <devpark29@gmail.com>
2024-02-20 18:50:22 +0900

    add eslint, prettier settings

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,33 @@
-import { OrganizationSwitcher } from "@clerk/nextjs";
+import { db } from "@/lib/db";
+import { FormEvent } from "react";
 
 const OrganizationIdPage = () => {
-  return <div>Org page</div>;
+  async function create(event: FormEvent<HTMLFormElement>) {
+    "use server";
+
+    const formData = new FormData(event.currentTarget);
+    const title = formData.get("title") as string;
+
+    await db.board.create({
+      data: {
+        title,
+      },
+    });
+  }
+
+  return (
+    <div>
+      <form onSubmit={create}>
+        <input
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
